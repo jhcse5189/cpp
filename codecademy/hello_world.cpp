243da8e4414ee98e7d5f0fdb3215c6562cc14a6e
2018009143 <jhcse5189@gmail.com>
2019-10-16 21:25:03 +0900

    day_1

---
+++ codecademy/hello_world.cpp
@@ -0,0 +1,64 @@
+/* Introduction to C++ */
+
+/* C++ is a programming language created by Bjarne Stroustrup 
+ * at Bell Laboratories in 1979.
+ * As the name implies, C++ was derived from the C language;
+ * Bjarne’s goal was to add object-oriented programming into C,
+ * a language well-respected for its portability and low-level functionality.
+ *
+ * It is fast and flexible.
+ * It is well-supported.
+ * It forces you to think in new and creative ways.
+ */
+
+
+
+// include libraries
+#include <iostream>
+
+int main() {
+
+    // "cout" means "character output stream", is pronounced "see-out".
+    // "<<" operator == comes right after it.
+    std::cout << "Hello World!\n\n";
+
+    std::cout << "Dear Self,\n";
+    std::cout << "Getting used to c++ rather than c!\n";
+    std::cout << "10/16/2019\n";
+
+    std::cout << "    *                   *        \n";
+    std::cout << "*        *                       \n";
+    std::cout << " *                *            * \n";
+    std::cout << "      *     *                    \n";
+    std::cout << "                      *          \n";
+    std::cout << "*         *                 *    \n";
+    std::cout << "     * 	      *     *          \n";
+    std::cout << "           HHHHHHH        *      \n";
+    std::cout << " *         HHHHHHH               \n";
+    std::cout << "     *   HHHHHHHHHHH    *     *  \n";
+    std::cout << "         *   @ @   *             \n";
+    std::cout << "        *     ^     *            \n";
+    std::cout << "  *       *       *         *    \n";
+    std::cout << "         SSS * * S               \n";
+    std::cout << "         * SSSSSSSS *            \n";
+    std::cout << "       *      O   SS  *          \n";
+    std::cout << "      *       O    S   *         \n";
+    std::cout << "      *       O        *         \n";
+    std::cout << "       *              *          \n";
+    std::cout << "           *   *   *             \n";
+
+
+    // a value of 0 is an indication to the operating system
+    //that the code executed successfully
+
+    /* cpp's primitive data type */
+
+    /* int: integer numbers
+     * double: floating-point numbers
+     * char: individual characters
+     * string: a sequence of characters
+     * bool: true/false values
+     */
+
+    return 0;
+}
